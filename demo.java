87bda73219219bcb4ae69849f4e6ec48ae1202dc
mounikabandi123 <mounikabandi50@gmail.com>
2021-02-04 10:18:42 +0000

    code merged

--- demo.java
+++ demo.java
@@ -1,3 +1,19 @@
+class main code ( )
+{
+ s.o.print ("main code");
+ s.o.print ("main code");
+ s.o.print ("main code");
+ s.o.print ("main code");
+} 
+
+class branchcode
+{
+ s.o.p("branch code");
+ s.o.p("branch code");
+ s.o.p("branch code");
+ s.o.p("branch code");
+}
+
 class demo ( )
 {
  public static void main (....)
